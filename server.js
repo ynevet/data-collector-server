f1b3f8d24b4682cb76c27e347201808af16c135c
Yair Nevet <ynevet@chegg.com>
2019-12-23 12:42:03 +0200

    add docker files

--- server.js
+++ server.js
@@ -2,25 +2,41 @@ const express = require('express');
 const app = express();
 const uuid = require('uuid');
 const cookieParser = require('cookie-parser');
+const path = require('path');
+const public = path.join(__dirname, 'public');
+const redis = require("redis");
+const redisClient = redis.createClient();
+
+
+redisClient.on("error", function (err) {
+    console.log("Redis Error: " + err);
+});
 
 app.use(cookieParser());
 
-app.get('/', function (req, res) {
-  res.send('index')
-})
+app.use('/', express.static(public));
 
 app.get('/collect', function (req, res) {
     if(!req.cookies.sessionID){
         let sessionId = uuid.v1();
         res.cookie('sessionID', sessionId);
+        console.log('new session');
+
+        redisClient.set(sessionId, 'user session data', 'EX', 60, function(err) {
+            if (err) throw err;
+        });
+        
+
         //redis-create new entry of 5 minutes ttl for key sessionId with query params data
     }else{
-        //redis-update
-        //if not exist at redis(expired), then create seesionID
+        redisClient.get(req.cookies.sessionID, function (err, res) {
+            if (err) throw err;
+            if(res != null){console.log('found data for session:', res)}
+        });
     }
 
 
-    res.send(requestId);
+    res.send();
   })
  
 app.listen(3000)
