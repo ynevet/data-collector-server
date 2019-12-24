7d43f0882f9bde969c4dcd782b3234d91a9664e8
Yair Nevet <ynevet@chegg.com>
2019-12-24 17:33:15 +0200

    set cookie options

--- middlewares/handleSessionState.js
+++ middlewares/handleSessionState.js
@@ -1,20 +1,21 @@
 const uuid = require('uuid');
 const SESSION_COOKIE = 'sessionID';
-const SESSION_TTL_IN_SECONDS = 5*60;
+const SESSION_TTL_IN_SECONDS = 60 * 5;
+const SESSION_COOKIE_OPTIONS = { httpOnly: true, secure: false, maxAge: SESSION_TTL_IN_SECONDS * 1000 };
 
 module.exports.handleSessionStateAsync = (redisClient) => {
     return async (req, res, next) => {
         let sessionId;
         if (req.cookies[SESSION_COOKIE] === undefined) {
             sessionId = uuid.v1();
-            res.cookie(SESSION_COOKIE, sessionId, { httpOnly: true });
+            res.cookie(SESSION_COOKIE, sessionId, SESSION_COOKIE_OPTIONS);
         }
         else {
             sessionId = req.cookies[SESSION_COOKIE];
             const sessionData = await redisClient.getAsync(sessionId);
             if (!sessionData) {
                 sessionId = uuid.v1();
-                res.cookie(SESSION_COOKIE, sessionId, { httpOnly: true });
+                res.cookie(SESSION_COOKIE, sessionId, SESSION_COOKIE_OPTIONS);
             }
         }
 
